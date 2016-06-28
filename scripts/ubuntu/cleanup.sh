fbde638edcd05b9bc1c7a8f299fd2d3f49ca11d6
Marko Martinović <marko@techytalk.info>
2016-06-28 13:32:18 +0200

    Fix cleaning old kernels for Linux kernels > 3.x

--- scripts/ubuntu/cleanup.sh
+++ scripts/ubuntu/cleanup.sh
@@ -6,14 +6,10 @@ dpkg --list \
   | grep 'linux-headers' \
   | xargs apt-get -y purge;
 
-# Remove specific Linux kernels, such as linux-image-3.11.0-15-generic but
-# keeps the current kernel and does not touch the virtual packages,
-# e.g. 'linux-image-generic', etc.
-dpkg --list \
-    | awk '{ print $2 }' \
-    | grep 'linux-image-3.*-generic' \
-    | grep -v `uname -r` \
-    | xargs apt-get -y purge;
+# Remove old Linux kernels
+dpkg -l 'linux-*' | sed '/^ii/!d;/'"$(uname -r | \
+sed "s/\(.*\)-\([^0-9]\+\)/\1/")"'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d' | \
+xargs apt-get -y purge
 
 # Delete Linux source
 dpkg --list \
